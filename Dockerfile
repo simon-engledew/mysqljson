8a558e78c2cac9f2b9397db6d491ca42d7cde0b5
Simon Engledew <simon-engledew@github.com>
2022-03-08 09:45:24 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM debian:11.2-slim AS build
+
+RUN apt-get update && apt-get install --no-install-recommends -y curl ca-certificates
+
+WORKDIR /workspace/mysql-server/boost
+
+RUN curl -L http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz -O && \
+  tar xfz boost_1_59_0.tar.gz
+
+WORKDIR /workspace/mysql-server
+
+RUN curl -L https://github.com/mysql/mysql-server/archive/refs/tags/mysql-5.7.37.tar.gz | \
+  tar xvpz --strip-components=1
+
+RUN apt-get update && apt-get install --no-install-recommends -y build-essential cmake libssl-dev libncurses5-dev bison pkg-config
+
+RUN cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=true -DWITH_BOOST=boost
+
+RUN make sql binlog rpl master slave
+
+RUN echo 'MYSQL_ADD_EXECUTABLE(mysqljson mysqljson.cc)' >> sql/CMakeLists.txt \
+    && echo 'TARGET_LINK_LIBRARIES(mysqljson sql binlog rpl master slave)' >> sql/CMakeLists.txt
+
+COPY src/ ./
+
+RUN cmake . -DCMAKE_BUILD_TYPE=Release -DWITH_BOOST=boost
+
+RUN make mysqljson
+
+RUN strip sql/mysqljson
+
+FROM debian:11.2-slim
+
+COPY --from=build /workspace/mysql-server/sql/mysqljson /usr/bin/mysqljson
+
+ENTRYPOINT [ "/usr/bin/mysqljson" ]
---
+++ src/sql/mysqljson.cc
@@ -0,0 +1,53 @@
+#undef WITH_PERFSCHEMA_STORAGE_ENGINE
+
+#include <iostream>
+#include "sql_class.h"
+#include "opt_costconstantcache.h"
+#include "item_json_func.h"
+#include <iterator>
+
+#ifdef WITH_PERFSCHEMA_STORAGE_ENGINE
+#include "../storage/perfschema/pfs_server.h"
+#endif /* WITH_PERFSCHEMA_STORAGE_ENGINE */
+
+int main(int _argc, char **_argv)
+{
+  system_charset_info= &my_charset_utf8mb4_bin;
+
+  const char * name = "mysqljson";
+
+  char *argv[] = { const_cast<char*>(name), 0 };
+  set_remaining_args(1, argv);
+
+  #ifdef WITH_PERFSCHEMA_STORAGE_ENGINE
+  pre_initialize_performance_schema();
+  #endif /*WITH_PERFSCHEMA_STORAGE_ENGINE */
+
+  MY_INIT(name);
+
+  sys_var_init();
+  init_common_variables();
+  randominit(&sql_rand, 0, 0);
+  transaction_cache_init();
+  init_optimizer_cost_module(false);
+
+  THD m_thd(false);
+  m_thd.set_new_thread_id();
+  m_thd.thread_stack= (char*) &m_thd;
+  m_thd.store_globals();
+
+  std::istream_iterator<char> it(std::cin);
+  std::istream_iterator<char> end;
+  std::string value(it, end);
+
+  Json_dom *dom = Json_dom::parse(value.data(), value.length(), NULL, NULL, false);
+  if (dom != NULL) {
+    String buffer;
+    Json_wrapper w(dom);
+    w.to_string(&buffer, true, "format");
+
+    std::cout << std::string(buffer.ptr(), buffer.length()) << std::endl;
+  }
+
+  m_thd.cleanup_after_query();
+}
